b6bbe28f66b68dbdc6f88c25056393f7f664b140
Balazs Perlaki-Horvath <ba.perlaki@gmail.com>
2021-06-19 13:58:21 +0200

    Initial buildable project

---
+++ IrishInterest/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  IrishInterest
+//
+//  Created by Balazs Perlaki-Horvath on 19/06/2021.
+//
+
+import Foundation
+import UIKit
+
+final class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
