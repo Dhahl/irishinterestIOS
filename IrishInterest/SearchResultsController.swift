ebdee1c14866e3f1e05b7d0b5e232df0e6dd99c5
Balazs Perlaki-Horvath <ba.perlaki@gmail.com>
2021-06-19 21:46:59 +0200

    Add search controller

---
+++ IrishInterest/SearchResultsController.swift
@@ -0,0 +1,8 @@
+// Copyright © 2021 Balazs Perlaki-Horvath, PerlakiDigital. All rights reserved.
+
+import Foundation
+import UIKit
+
+final class SearchResultsController: UIViewController {
+
+}
--- IrishInterest/TabViewController.swift
+++ IrishInterest/TabViewController.swift
@@ -9,16 +9,16 @@ final class TabViewController: UITabBarController {
         super.viewDidLoad()
         title = "IrishInterest"
         view.backgroundColor = .white
+        
+        // NAV LEFT
         let burgerImage = UIImage(systemName: "line.horizontal.3")
         let barButton = UIBarButtonItem(image: burgerImage, style: .plain, target: nil, action: nil)
         barButton.tintColor = Brand.colorTabSelected
         navigationItem.leftBarButtonItem = barButton
         
-        let searchButton = UIBarButtonItem(barButtonSystemItem: .search, target: nil, action: nil)
-        searchButton.tintColor = Brand.colorTabSelected
-        navigationItem.rightBarButtonItem = searchButton
+        navigationItem.searchController = UISearchController()
         
-        
+        // TAB BAR
         tabBar.backgroundColor = Brand.colorTabBarBackground
         tabBar.unselectedItemTintColor = Brand.colorTabUnselected
         tabBar.tintColor = Brand.colorTabSelected
