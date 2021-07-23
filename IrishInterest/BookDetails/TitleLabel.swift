2b7775cb76c324dc652230a4b36d604e07bfec41
Balazs Perlaki-Horvath <ba.perlaki@gmail.com>
2021-07-23 23:55:49 +0200

    Update title labels for book details

--- IrishInterest/BookDetails/TitleLabel.swift
+++ IrishInterest/BookDetails/TitleLabel.swift
@@ -10,8 +10,8 @@ struct TitleLabel {
     func display(in contentView: UIView, with stack: VStack, using border: CGFloat) {
         let title = UILabel()
         let value = UILabel()
-        UI.format(.body, color: .label, label: title, text: titleText, nrOfLines: 1)
-        UI.format(.body, color: .label, label: value, text: valueText, nrOfLines: 1)
+        UI.format(.subheadline, color: .secondaryLabel, label: title, text: titleText, nrOfLines: 1)
+        UI.format(.subheadline, color: .secondaryLabel, label: value, text: valueText, nrOfLines: 1)
         UI.fit(title, to: contentView, left: border)
         title.widthAnchor.constraint(equalTo: contentView.widthAnchor, constant: -border).isActive = true
         UI.fit(value, to: contentView, left: border)
